2b27f3c53daed8a47b7763a22e5282dae9fdaba6
uuzjin <lyj5721@sookmyung.ac.kr>
2026-08-26 23:50:41 +0900

    #1 Feat: 메인 페이지에 헤더 렌더링

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
+import Header from "@/components/layout/Header";
+import Button from "@/components/common/Button";
+
 export default function MainPage() {
   // '/onboarding'으로 이동
   // redirect("/onboarding");
+
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
