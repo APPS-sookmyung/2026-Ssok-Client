f8582d5dfd50ec6d9c6dcc55da07e11281a83d2f
uuzjin <lyj5721@sookmyung.ac.kr>
2026-08-27 01:44:20 +0900

    #1 Feat: 메인 페이지 사이드바 반영

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,29 @@
 import Header from "@/components/layout/Header";
-import Button from "@/components/common/Button";
+import Sidebar from "@/components/layout/Sidebar";
 
 export default function MainPage() {
   // '/onboarding'으로 이동
   // redirect("/onboarding");
 
   return (
-    <div>
-      <Header />
+    <div className="flex h-screen w-screen flex-col overflow-hidden bg-white">
+      {/* 1. 상단 헤더 */}
+      <Header variant="default" />
+
+      {/* 2. 하단 본문 */}
+      <div className="flex flex-1 overflow-hidden">
+        {/* 좌측 사이드바 영역 */}
+        <aside className="shrink-0 border-r border-gray-100 bg-white overflow-y-auto">
+          <Sidebar />
+        </aside>
+
+        {/* 우측 메인 콘텐츠 영역 */}
+        <main className="flex-1 overflow-y-auto bg-gray-50/30 p-8">
+          <div className="flex h-full items-center justify-center">
+            <p>lorem ipsum</p>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
